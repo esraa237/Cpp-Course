3dc3230df3015e22145f40fa944fa5ae013d5c94
esraa237 <esraafoda2000@gmail.com>
2022-08-04 03:55:33 +0200

    edit cpp file in task1 section4

--- Section4_Datatypes&Variables/easy/task1/main.cpp
+++ Section4_Datatypes&Variables/easy/task1/main.cpp
@@ -2,11 +2,7 @@
 /*Problem #1: Math operations
 ● Write a program that reads 2 numbers and print their + - * / as following
 ○ For inputs 12 and 3
-● Do good testing for your code
-○ E.g. consider zero as first or 2nd number
-○ E.g. consider negative values
-○ E.g. even and odd values
-○ E.g. try the MAX of int: 2147483647*/
+*/
 
 #include <iostream>
 
