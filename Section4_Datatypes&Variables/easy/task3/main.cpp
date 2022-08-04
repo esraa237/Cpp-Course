da2b8a619ebe3681c3b390f0b810843d3479d70a
esraa237 <esraafoda2000@gmail.com>
2022-08-04 04:32:18 +0200

    finish task1 meduim section4

--- Section4_Datatypes&Variables/easy/task3/main.cpp
+++ Section4_Datatypes&Variables/easy/task3/main.cpp
@@ -12,8 +12,7 @@ using namespace std;
 
 int main()
 {
-    int num1,num2,num3,num4,num5,num6,num7,num8;
-    cin>>num1>>num2>>num3>>num4>>num5>>num6>>num7>>num8;
+    int num1,num2,num3,num4,num5,num6,num7,num8,cin>>num1>>num2>>num3>>num4>>num5>>num6>>num7>>num8;
     cout<<(num2+num4+num6+num8)<<" " <<(num1+num3+num5+num7);
 
 
---
+++ Section4_Datatypes&Variables/meduim/task1/main.cpp
@@ -0,0 +1,34 @@
+// Homework 4: Print Program Output
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, num3;
+
+	num1 = 0, num2 = 1, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+
+	// https://en.wikipedia.org/wiki/Fibonacci_number
+
+	return 0;
+}
+
+/*answer
+1
+2
+3
+5
+8
+13
+21
+34
+55
+*/
